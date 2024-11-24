f4e69d29ff5c841ff9c1d82c611d55d6a88b99e1
trung0501 <nguyentrungt151@gmail.com>
2024-11-24 23:04:38 +0700

    datafirst

---
+++ OnTap_LTDN/Controllers/DiemController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnTap_LTDN.Models;
+
+namespace OnTap_LTDN.Controllers
+{
+    public class DiemController : Controller
+    {
+        private QLSVDataContext db = new QLSVDataContext();
+
+        // GET: Diem
+        public ActionResult Index(string searchString)
+        {
+            var diems = db.Diems.Include(d => d.SinhVien);
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                searchString = searchString.Trim().ToLower();
+                diems = diems.Where(d=> d.SinhVien.hoten.Trim().ToLower().Contains(searchString));
+            }
+            return View(diems.ToList());
+        }
+
+        // GET: Diem/Details/5
+        public ActionResult Details(int? id, string tenmh)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Diem diem = db.Diems.Find(id, tenmh);
+            if (diem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(diem);
+        }
+
+        // GET: Diem/Create
+        public ActionResult Create()
+        {
+            ViewBag.masv = new SelectList(db.SinhViens, "masv", "hoten");
+            return View();
+        }
+
+        // POST: Diem/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "masv,tenmh,diem1")] Diem diem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Diems.Add(diem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.masv = new SelectList(db.SinhViens, "masv", "hoten", diem.masv);
+            return View(diem);
+        }
+
+        // GET: Diem/Edit/5
+        public ActionResult Edit(int? id, string tenmh)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Diem diem = db.Diems.Find(id, tenmh);
+            if (diem == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.masv = new SelectList(db.SinhViens, "masv", "hoten", diem.masv);
+            return View(diem);
+        }
+
+        // POST: Diem/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "masv,tenmh,diem1")] Diem diem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(diem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.masv = new SelectList(db.SinhViens, "masv", "hoten", diem.masv);
+            return View(diem);
+        }
+
+        // GET: Diem/Delete/5
+        public ActionResult Delete(int? id, string tenmh)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Diem diem = db.Diems.Find(id, tenmh);
+            if (diem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(diem);
+        }
+
+        // POST: Diem/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id, string tenmh)
+        {
+            Diem diem = db.Diems.Find(id, tenmh);
+            db.Diems.Remove(diem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OnTap_LTDN/Controllers/SinhVienController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OnTap_LTDN.Models;
+
+namespace OnTap_LTDN.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        private QLSVDataContext db = new QLSVDataContext();
+
+        // GET: SinhVien
+        public ActionResult Index()
+        {
+            return View(db.SinhViens.ToList());
+        }
+
+        public ActionResult SVDiemKTLTThapnhap()
+        {
+            //Lấy điểm thấp nhất của môn học KTLT
+            var diemmin = db.Diems
+                .Where(d => d.tenmh.Equals("Kỹ thuật lập trình"))
+                .Min(d => d.diem1);
+            //Lấy ds sv có điểm KTLT cao nhất
+            var dsmmin = db.Diems
+                .Where(d => d.tenmh.Equals("Kỹ thuật lập trình") && d.diem1 == diemmin)
+                .Select(d => d.SinhVien)
+                .ToList();
+            return View(dsmmin);
+        }
+
+        // GET: SinhVien/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhVien);
+        }
+
+        // GET: SinhVien/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SinhVien/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "masv,hoten,namsinh,gioitinh,email")] SinhVien sinhVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SinhViens.Add(sinhVien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sinhVien);
+        }
+
+        // GET: SinhVien/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhVien);
+        }
+
+        // POST: SinhVien/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "masv,hoten,namsinh,gioitinh,email")] SinhVien sinhVien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sinhVien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sinhVien);
+        }
+
+        // GET: SinhVien/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            if (sinhVien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sinhVien);
+        }
+
+        // POST: SinhVien/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            SinhVien sinhVien = db.SinhViens.Find(id);
+            db.SinhViens.Remove(sinhVien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
